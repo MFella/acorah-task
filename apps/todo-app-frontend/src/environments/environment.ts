bd551576a0090157495846bba7e532aafe821bef
MFella <mfella@wp.pl>
2026-06-09 09:40:23 +0200

    Change of env import in api url token

--- apps/todo-app-frontend/src/environments/environment.ts
+++ apps/todo-app-frontend/src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
     production: true,
-    apiUrl: 'https://todo-app-backend-production-f23a.up.railway.app'
+    apiUrl: 'https://todo-app-backend-production-f23a.up.railway.app/api'
 };
